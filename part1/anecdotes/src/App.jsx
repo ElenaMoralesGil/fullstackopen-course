cbfdc509eeb1df593b9d1d728996877a56cb18d4
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-07-07 16:30:53 +0100

    anecdotes part1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,58 +1,30 @@
 import { useState } from 'react'
 
-
-const Header = props => {
-    return (
-        <h1>{props.text}</h1>
-    )
-}
-const Button = (props) => {
-    return (
-        <button onClick={props.onClick}>{props.text}</button>
-    )
-}
-const Statistic = (props) => {
-
-    return (
-        <tr>
-            <td>{props.text}</td>
-            <td>{props.value}</td>
-        </tr>
-    )
-}
 const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const all = good + neutral + bad;
-    const average = (good - bad ) / all || 0;
-    const positive = (good / all) * 100 || 0;
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ]
 
+    const [selected, setSelected] = useState(0)
+
+    const getRandomAnecdote = () => {
+        const randomIndex = Math.floor(Math.random() * anecdotes.length);
+        setSelected(randomIndex);
+    };
 
     return (
         <div>
-            <Header text={"give feedback"}/>
-            <Button onClick={() => setGood(good + 1)} text="good"/>
-            <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
-            <Button onClick={() => setBad(bad + 1)} text="bad"/>
-            <Header text={"statistics"}/>
-            {all === 0 ? (
-                <p>No feedback given</p>
-            ) : (
-                <table>
-                    <tbody>
-                    <Statistic text="good" value={good}/>
-                    <Statistic text="neutral" value={neutral}/>
-                    <Statistic text="bad" value={bad}/>
-                    <Statistic text="all" value={all}/>
-                    <Statistic text="average" value={average.toFixed(2)}/>
-                    <Statistic text="positive" value={positive.toFixed(2) + ' %'}/>
-                    </tbody>
-                </table>
-            )}
+            <p>{anecdotes[selected]}</p>
+            <button onClick={getRandomAnecdote}>Next Anecdote</button>
         </div>
-    )
+    );
 }
 
 export default App
